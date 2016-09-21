07eff4144fac9185484cba57765435184688f74a
xxping90 <xxping90@126.com>
2016-09-21 17:04:59 +0800

    第二次提交

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVistorTest(unittest.TestCase):
@@ -7,11 +8,41 @@ class NewVistorTest(unittest.TestCase):
 		self.browser.implicitly_wait(3)
 	def tearDown(self):
 		self.browser.quit()
+	def check_for_row_in_list_table(self,row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name("tr")
+		self.assertIn(row_text,[row.text for row in rows])
+
 	def test_can_start_a_list_and_rerieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
-		self.assertIn('Django',self.browser.title)
-		self.fail('finish the test!')
+		self.assertIn('To-Do',self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do',header_text)
+
+		#应用邀请她输入一个待办事项
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+
+		self.check_for_row_in_list_table('1:Buy peacock feathers')
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		self.check_for_row_in_list_table('1:Buy peacock feathers')
+		self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+		# self.fail('finish the test!')
+
 
 if __name__=='__main__':
 	unittest.main()
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$','lists.views.home_page',name='home'),
 ]
